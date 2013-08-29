07006067a59fe93f355d775c216278a15a74336a
Silviu ANDRICA <=>
2013-08-29 18:15:26 +0200

    two

---
+++ SwengTutorial/src/com/example/swengtutorial/ShowMessageActivity.java
@@ -0,0 +1,24 @@
+package com.example.swengtutorial;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ShowMessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show_message);
+		Intent startingIntent = getIntent();
+		String userText = startingIntent.getStringExtra(MainActivity.class.getName());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.show_message, menu);
+		return true;
+	}
+
+}
